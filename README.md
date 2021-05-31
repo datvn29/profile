e7cf3c3b7bc6848a09fea5e4084cf7c4c2e5f486
Dat <73944950+datvn29@users.noreply.github.com>
2021-05-31 17:03:29 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# dat2912.github.io
---
+++ script.js
@@ -0,0 +1,8 @@
+function mode() {
+   var element = document.body;
+   element.classList.toggle("dark-mode");
+}
+function more() {
+   var element = document.body;
+   element.classList.toggle("smalltab");
+}
